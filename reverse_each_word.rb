7b141c3dd9a58839761f3f3a6964c04639ed643a
Kevin Neyer <kevinneyer@gmail.com>
2020-04-15 17:06:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def reverse_each_word(string)
- message = string.split
+ message = string.split(" ")
  updated = message.map{ |s| s.reverse}
- try_me = " "
+ try_me = ""
  updated.each do |x|
-   try_me += x.to_s + " "
+   try_me += x.to_s +
  end 
  try_me
 end 
